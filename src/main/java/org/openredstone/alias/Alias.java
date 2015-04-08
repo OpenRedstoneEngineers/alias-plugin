fe536aa1e433f045b38c16e3cf925e0029b1f79e
mort <martid0311@gmail.com>
2015-04-08 18:40:37 +0200

    Command to reload config, and $n+ variable which is $n and all subsequent arguments, and magical __join and __quit command names which are triggered when a player joins or quits

--- src/main/java/org/openredstone/alias/Alias.java
+++ src/main/java/org/openredstone/alias/Alias.java
@@ -3,6 +3,7 @@ package org.openredstone.alias;
 import java.util.logging.Logger;
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.lang.Iterable;
@@ -16,15 +17,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
 public class Alias extends JavaPlugin implements Listener
 {
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public final PluginDescriptionFile descFile = this.getDescription();
-	public final Map aliases = this.getConfig()
-		.getConfigurationSection("aliases")
-		.getValues(false);
+	public Map aliases;
 
 	@Override
 	public void onEnable()
@@ -32,13 +33,9 @@ public class Alias extends JavaPlugin implements Listener
 		this.saveDefaultConfig();
 
 		getServer().getPluginManager().registerEvents(this, this);
-
 		logger.info(descFile.getName()+" enabled.");
-		logger.info("Aliases: ");
-		for (Map.Entry entry : (Iterable<Map.Entry>)aliases.entrySet())
-		{
-			logger.info(" - "+entry.getKey());
-		}
+
+		loadConfig();
 	}
 
 	@Override
@@ -47,8 +44,39 @@ public class Alias extends JavaPlugin implements Listener
 		logger.info(descFile.getName()+" disabled.");
 	}
 
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel,  String[] args)
+	{
+		if (args.length < 1)
+		{
+			sender.sendMessage("Usage: alias <reload>");
+			return false;
+		}
+
+		if (cmd.getName().equalsIgnoreCase("alias"))
+		{
+			if (args[0].equals("reload"))
+			{
+				if (hasPermission(sender, "alias.reload"))
+				{
+					reloadConfig();
+					loadConfig();
+					sender.sendMessage("Reloaded config.");
+
+					return true;
+				}
+				else
+				{
+					sender.sendMessage("You don't have permission to do that.");
+				}
+			}
+		}
+
+		return false;
+	}
+
 	@EventHandler
-	public void preCommandPlayer(PlayerCommandPreprocessEvent evt)
+	public void onPreCommandPlayer(PlayerCommandPreprocessEvent evt)
 	{
 		String message = evt.getMessage().substring(1);
 		String[] tokens = message.split("( )+");
@@ -61,24 +89,87 @@ public class Alias extends JavaPlugin implements Listener
 		evt.setCancelled(true);
 		evt.setMessage("/");
 
+		execAliasParts(aliasParts, tokens, player);
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent evt)
+	{
+		if (!aliases.containsKey("__join"))
+			return;
+
+		List<String> aliasParts = (List<String>)(aliases.get("__join"));
+		execAliasParts(aliasParts, null, evt.getPlayer());
+	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent evt)
+	{
+		if (!aliases.containsKey("__quit"))
+			return;
+
+		List<String> aliasParts = (List<String>)(aliases.get("__quit"));
+		execAliasParts(aliasParts, null, evt.getPlayer());
+	}
+
+	private void loadConfig()
+	{
+		aliases = getConfig()
+			.getConfigurationSection("aliases")
+			.getValues(false);
+
+		logger.info("Aliases: ");
+		for (Map.Entry entry : (Iterable<Map.Entry>)aliases.entrySet())
+		{
+			logger.info(" - "+entry.getKey());
+		}
+	}
+
+	private boolean hasPermission(CommandSender sender, String perm)
+	{
+		if (sender instanceof Player)
+		{
+			if (((Player)sender).hasPermission(perm))
+				return true;
+			else
+				return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	private void execAliasParts(List<String> aliasParts, String[] tokens, Player player)
+	{
+		List<String> commands = new ArrayList<String>();
 		try
 		{
 			for (String aliasPart : aliasParts)
 			{
+				if (aliasPart.startsWith("{no-op}"))
+					continue;
+
 				String cmd = parseAliasPart(aliasPart, tokens, player);
-				logger.info(cmd);
-				player.performCommand(cmd);
+				logger.info("Alias: '"+aliasPart+"' became '"+cmd+"'");
+				commands.add(cmd);
 			}
 		}
 		catch (Exception e)
 		{
 			player.sendMessage(e.getMessage());
+			return;
+		}
+
+		for (String cmd : commands)
+		{
+			player.performCommand(cmd);
 		}
 	}
 
 	private String parseAliasPart(String aliasPart, String[] tokens, Player player) throws Exception
 	{
-		Matcher argMatcher = Pattern.compile("\\$([0-9]+)").matcher(aliasPart);
+		Matcher argMatcher = Pattern.compile("\\$([0-9]+)(?:(\\+))?").matcher(aliasPart);
 		while (argMatcher.find())
 		{
 			int num = Integer.valueOf(argMatcher.group(1));
@@ -86,7 +177,22 @@ public class Alias extends JavaPlugin implements Listener
 			if (num >= tokens.length)
 				throw new Exception("You are missing some arguments.");
 
-			aliasPart = aliasPart.replace(argMatcher.group(0), tokens[num]);
+			if (argMatcher.group(2).equals("+"))
+			{
+				String res = "";
+
+				for (int i = num; i < tokens.length; ++i)
+				{
+					res += " "+tokens[i];
+				}
+
+				res = res.substring(1);
+				aliasPart = aliasPart.replace(argMatcher.group(0), res);
+			}
+			else
+			{
+				aliasPart = aliasPart.replace(argMatcher.group(0), tokens[num]);
+			}
 		}
 
 		Matcher varMatcher = Pattern.compile("\\{([a-zA-Z\\.]+)\\}").matcher(aliasPart);
